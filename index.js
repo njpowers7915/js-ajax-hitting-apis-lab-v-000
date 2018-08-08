3eac9f3b84d874f7b201b897ac22359c15483217
Nicholas Powers <njpowers7915@gmail.com>
2018-08-08 15:02:39 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,31 @@
-function getRepositories() {
+function getRepositories(username) {
   const req = new XMLHttpRequest()
-  req.open("GET", 'https://api.github.com/users/njpowers7915/repos')
+  req.addEventListener("load", displayRepositories)
+  req.open("GET", 'https://api.github.com/users/' + string(username) + '/repos')
   req.send()
 }
+
+function displayRepositories(event, data) {
+  var repos = JSON.parse(this.responseText)
+  console.log(repos)
+  const repoList = `<ul>${repos.map(r => '<li>' + r.name + ' - <a href="#" data-repo="' + r.name + '" onclick="getCommits(this)">Get Commits</a></li>').join('')}</ul>`
+  document.getElementById("repositories").innerHTML = repoList
+}
+
+function getCommits(username, repository) {
+  const name = el.dataset.repo
+  const req = new XMLHttpRequest()
+  req.addEventListener("load", displayCommits)
+  req.open("GET", 'https://api.github.com/repos/octocat/' + name + '/commits')
+  req.send()
+}
+
+function displayCommits(){
+  const commits = JSON.parse(this.responseText)
+  const commitsList = `<ul>${commits.map(commit => '<li><strong>' + commit.author.login + '</strong> - ' + commit.commit.message + '</li>').join('')}</ul>`
+  document.getElementById("commits").innerHTML = commitsList
+}
+
+function getBranches(username, repository) {}
+
+function displayBranches(){}
