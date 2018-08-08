8816fc412c6d39db3ec312714cd8d7a6ccbb040a
Nicholas Powers <njpowers7915@gmail.com>
2018-08-08 16:08:37 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function getRepositories(username) {
-  const req = new XMLHttpRequest()
-  req.addEventListener("load", displayRepositories)
-  req.open("GET", 'https://api.github.com/users/' + string(username) + '/repos')
+  const req = new XMLHttpRequest(username)
+  req.addEventListener("load", displayRepositories("submit", username))
+  req.open("GET", "https://api.github.com/users" + username + "/repos")
   req.send()
 }
 
