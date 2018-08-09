7b91c776be42d741b066a9a1caf443dc5843e141
Nicholas Powers <njpowers7915@gmail.com>
2018-08-09 17:06:19 +0000

    Done.

--- index.js
+++ index.js
@@ -9,24 +9,40 @@ function getRepositories() {
 function displayRepositories(event, data) {
   var repos = JSON.parse(this.responseText)
   console.log(repos)
-  const repoList = `<ul>${repos.map(r => '<li>' + r.name + ' - <a href="#" data-repo="' + r.name + '" onclick="getCommits(this)">Get Commits</a></li>').join('')}</ul>`
+  const repoList = `<ul>${repos.map(r => '<li>' + r.name + ' - <a href="#" data-repo="' +
+  r.name + '" onclick="getCommits(this)">Get Commits</a></li>' + ' - <a href="#" data-repo="' +
+  r.name + '" onclick="getBranches(this)">Get Branches</a></li>').join('')}</ul>`
   document.getElementById("repositories").innerHTML = repoList
 }
 
-function getCommits(username, repository) {
-  const name = el.dataset.repo
+function getCommits(el) {
+  const name = el.dataset.repository
   const req = new XMLHttpRequest()
+  const username = document.getElementById("username").value
   req.addEventListener("load", displayCommits)
-  req.open("GET", 'https://api.github.com/repos/octocat/' + name + '/commits')
+  req.open("GET", 'https://api.github.com/repos/' + username + '/' + name + '/commits')
   req.send()
 }
 
 function displayCommits(){
   const commits = JSON.parse(this.responseText)
+  console.log(commits)
   const commitsList = `<ul>${commits.map(commit => '<li><strong>' + commit.author.login + '</strong> - ' + commit.commit.message + '</li>').join('')}</ul>`
   document.getElementById("commits").innerHTML = commitsList
 }
 
-function getBranches(username, repository) {}
+function getBranches(el) {
+  const name = el.dataset.repository
+  const req = new XMLHttpRequest()
+  const username = document.getElementById("username").value
+  req.addEventListener("load", displayBranches)
+  req.open("GET", 'https://api.github.com/repos/' + username + '/' + name + '/branches')
+  req.send()
+}
 
-function displayBranches(){}
+function displayBranches(){
+  const branches = JSON.parse(this.responseText)
+  console.log(branches)
+  const branchesList = `<ul>${branches.map(branch => branch.name).join('')}</ul>`
+  document.getElementById("commits").innerHTML = branchesList
+}
