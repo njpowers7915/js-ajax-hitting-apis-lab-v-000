d6a0e9bb121546e2393605ffdfa13b140554ebdb
Nicholas Powers <njpowers7915@gmail.com>
2018-08-09 22:00:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function displayRepositories(event, data) {
   var repos = JSON.parse(this.responseText)
   console.log(repos)
   const repoList = `<ul>${repos.map(r => '<li>' + r.name + ' - <a href="#" data-repo="' +
-  r.name + '" onclick="getCommits(this)">Get Commits</a></li>' + ' - <a href="#" data-repo="' +
+  r.name + '" onclick="getCommits(this)">Get Commits</a>' + ' - <a href="#" data-repo="' +
   r.name + '" onclick="getBranches(this)">Get Branches</a></li>').join('')}</ul>`
   document.getElementById("repositories").innerHTML = repoList
 }
